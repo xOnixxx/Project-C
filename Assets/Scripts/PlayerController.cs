4d5858458d7e1e535c15b1e70a26b19abd8dcfec
Dominik Molnár <55879959@cuni.cz>
2023-05-03 18:28:54 +0200

    Add animation for entering cast mode

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private enum State
+    {
+        Idle,
+        Casting
+    }
     //Character stats
+    public bool inControl = true;
     private CharacterController character;
+    private Animator animator;
     public float gravity = 9.87f;
-    public float horizontalSpeed = 4;
-    private float verticalSpeed = 0;
     public float speed = 6;
+    public float sprintHorizontalSpeed = 10;
+    private float verticalSpeed = 0;
+    private float curSpeed = 6;
+    public float castModeCooldown = 1.5f;
+    private bool canChangeMode = true;
+    private State state = State.Idle;
 
     //Camera stats
     public Transform cameraHolder;
@@ -20,26 +31,28 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        CheckActions();
+        if (inControl)
+        {
+            Move();
+        }
         Rotate();
     }
 
     private void Move()
     {
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
-
-        if (character.isGrounded) 
+        if (character.isGrounded)
         {
             if (Input.GetButton("Jump"))
             {
-                verticalSpeed += horizontalSpeed;
+                verticalSpeed += speed;
             }
             else
             {
@@ -49,9 +62,15 @@ public class PlayerController : MonoBehaviour
         else { verticalSpeed -= gravity * Time.deltaTime; }
 
         Vector3 gravityVec = new Vector3(0, verticalSpeed, 0);
-        Vector3 moveVec = transform.forward * verticalMove + transform.right * horizontalMove;
+        Vector3 moveVec = Vector3.zero;
+        if (inControl)
+        {
+            float horizontalMove = Input.GetAxis("Horizontal");
+            float verticalMove = Input.GetAxis("Vertical");
+            moveVec = transform.forward * verticalMove + transform.right * horizontalMove;
+        }
 
-        character.Move(speed * Time.deltaTime * moveVec + Time.deltaTime * gravityVec);
+        character.Move(curSpeed * Time.deltaTime * moveVec + Time.deltaTime * gravityVec);
     }
     private void Rotate()
     {
@@ -65,5 +84,49 @@ public class PlayerController : MonoBehaviour
         if(currentRotation.x > 180) { currentRotation.x -= 360; }
         currentRotation.x = Mathf.Clamp(currentRotation.x, upLimit, downLimit);
         cameraHolder.localRotation = Quaternion.Euler(currentRotation);
+    }
+    private void CheckActions()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            curSpeed = sprintHorizontalSpeed; 
+        }
+        else 
+        {
+            curSpeed = speed;
+        }
+        if(Input.GetButtonDown("Fire2"))
+        {
+            if(state == State.Idle && canChangeMode)
+            {
+                canChangeMode = false;
+                StartCoroutine(Cooldown());
+                state = State.Casting;
+                inControl = false;
+                StartCoroutine(CurveUp(0));
+                animator.SetBool("inCastMode", true);
+            }
+            else
+            {
+                state = State.Idle;
+                inControl = true;
+                animator.SetBool("inCastMode", false);
+            }
+        }
+    }
+
+    private IEnumerator CurveUp(int depth)
+    {
+        yield return new WaitForSeconds(0.02f);
+        if (depth < 10)
+        {
+            character.Move(new Vector3(0, 0.3f, 0) + transform.forward / 10);
+            StartCoroutine(CurveUp(depth + 1));
+        }
+    }
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(castModeCooldown);
+        canChangeMode = true;
     }
 }

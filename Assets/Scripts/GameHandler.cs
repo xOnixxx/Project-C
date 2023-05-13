6f123d5476dc446ab4dd2e2921790570a7b91f2f
Dominik Molnár <55879959@cuni.cz>
2023-05-13 18:09:36 +0200

    Add basic enemy spawning

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -8,10 +8,16 @@ public class GameHandler : MonoBehaviour
     public float levelRange = 1000;
     public TerrainGenerator tergen;
     public WallGenerator walls;
+    public int meleeEnemyNumber = 20;
+    public int rangedEnemyNumber = 10;
+    public int levelsPassed = 0;
+    public List<GameObject> meleeEnemyTypes = new List<GameObject>();
+    public List<GameObject> rangedEnemyTypes = new List<GameObject>();
+    public List<Spell> spellsForEnemy = new List<Spell>();
     // Start is called before the first frame update
     void Start()
     {
-
+        SpawnEnemies();
     }
 
     // Update is called once per frame
@@ -22,6 +28,7 @@ public class GameHandler : MonoBehaviour
 
     public void Restart()
     {
+        levelsPassed++;
         CharacterController character = player.GetComponent<CharacterController>();
         character.enabled = false;
         transform.position = new Vector3(Random.Range(-levelRange, levelRange), 0, Random.Range(-levelRange, levelRange));
@@ -31,6 +38,28 @@ public class GameHandler : MonoBehaviour
         tergen.GenerateNewTerrain();
         walls.GenerateWalls();
         player.transform.position = transform.position + new Vector3(0, tergen.roughness + 1, 0);
+        SpawnEnemies();
         character.enabled = true;
+    }
+
+    public void SpawnEnemies()
+    {
+        //RANDOM BASED, FOR ENVIRONMENT SPECIAL REMAKE SCRIPT
+        for (int i = 0; i < meleeEnemyNumber; i++)
+        {
+            Vector2 randomXZ = Random.onUnitSphere * tergen.Size;
+            GameObject enemy = Instantiate(meleeEnemyTypes[Random.Range(0, meleeEnemyTypes.Count - 1)],
+                new Vector3(randomXZ.x, tergen.roughness, randomXZ.y), Quaternion.identity,transform);
+            enemy.GetComponent<MeleeEnemyBehaviour>().target = player.transform;
+        }
+        for (int i = 0; i < rangedEnemyNumber; i++)
+        {
+            Vector2 randomXZ = Random.onUnitSphere * tergen.Size;
+            GameObject enemy = Instantiate(rangedEnemyTypes[Random.Range(0, rangedEnemyTypes.Count - 1)],
+                new Vector3(randomXZ.x, tergen.roughness, randomXZ.y), Quaternion.identity);
+            RangedEnemyBehaviour enemyBeh = enemy.GetComponent<RangedEnemyBehaviour>();
+            enemyBeh.target = player.transform;
+            enemyBeh.spell = Instantiate(spellsForEnemy[Random.Range(0, spellsForEnemy.Count - 1)], Vector3.zero, Quaternion.identity,transform);
+        }
     }
 }

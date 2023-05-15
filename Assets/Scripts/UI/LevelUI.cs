53dfe9d281852986fcf2b1e1800e54524e8f2230
Dominik Molnár <55879959@cuni.cz>
2023-05-16 00:37:08 +0200

    Add tutorial message

--- Assets/Scripts/UI/LevelUI.cs
+++ Assets/Scripts/UI/LevelUI.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class LevelUI : MonoBehaviour
 {
     public Text levelNumber;
-    public Text levelInfo;
     public GameHandler infoSource;
 
     // Start is called before the first frame update
@@ -24,6 +23,5 @@ public class LevelUI : MonoBehaviour
     public void UpdateInfo()
     {
         levelNumber.text = "Level : " + (1 + infoSource.levelsPassed);
-        levelInfo.text = "Debug"; //ADD HAZARD INFO
     }
 }

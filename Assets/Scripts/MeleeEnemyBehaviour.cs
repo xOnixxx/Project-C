59e6425176cba794c0c69872d7900e39b6f3582e
Dominik Molnár <55879959@cuni.cz>
2023-05-16 18:21:15 +0200

    Add melee attack

--- Assets/Scripts/MeleeEnemyBehaviour.cs
+++ Assets/Scripts/MeleeEnemyBehaviour.cs
@@ -9,6 +9,11 @@ public class MeleeEnemyBehaviour : Character
     public Transform target;
     public float movementSpeed = 2;
     public float gravity = 9.87f;
+    public float attackRange = 1.5f;
+    public float damage = 10;
+    public ISpell.Element element;
+    public float burnDamage = 0;
+    public int burnTicks = 0;
     private float verticalSpeed;
     // Start is called before the first frame update
     void Start()
@@ -31,10 +36,19 @@ public class MeleeEnemyBehaviour : Character
         else { verticalSpeed -= gravity * Time.deltaTime; }
 
         Vector3 gravityVec = new Vector3(0, verticalSpeed, 0);
-        if (Vector3.Distance(transform.position,target.position) > 1)
+        if (Vector3.Distance(transform.position, target.position) > attackRange)
         {
             transform.LookAt(target);
             character.Move(movementSpeed * Time.deltaTime * transform.forward + gravityVec * Time.deltaTime);
+        }
+        else
+        {
+            HealthManager toHit = target.gameObject.GetComponent<HealthManager>();
+            if (toHit != null)
+            {
+                toHit.GetHit(damage, element, burnTicks, burnDamage);
+            }
+            Die();
         }
     }
     public override void Die()
